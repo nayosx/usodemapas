b561fbfb0f5515fe6410000c83e257f2b3e4b263
NayosX <nayos144@gmail.com>
2015-09-01 23:48:29 -0600

    Ya un poco mas preparada la libreria final en comparacion de la anterior, esta ya es un 90% javascript

--- UsoDeGoogleMap/Scripts/map.js
+++ UsoDeGoogleMap/Scripts/map.js
@@ -13,31 +13,31 @@ var idDivMap = null; //name of id map-canvas
 var inputsearch = null; //name of id search
 var map = null;
 var mMarker = null;
-var setIdDivMap = function(idDivMap){
+var setIdDivMap = function (idDivMap) {
     this.idDivMap = idDivMap;
 };
-var setIdInputSearch = function(inputsearch){
+var setIdInputSearch = function (inputsearch) {
     this.inputsearch = inputsearch;
 };
-var setIdsLatlng = function(lat, lng){
+var setIdsLatlng = function (lat, lng) {
     this.lat = document.getElementById(lat);
     this.lng = document.getElementById(lng);
 };
 
-var asigLatLng = function(location){
-    if(isNaN(location.lat())){
+var asigLatLng = function (location) {
+    if (isNaN(location.lat())) {
         //location SV
         this.latitud = 13.794185;
         this.longitud = -88.89652999999998;
-    }else{
+    } else {
         this.lat.value = location.lat();
         this.lng.value = location.lng();
     }
 };
 
 //marker on map
-var placeMarker = function(location, map, marker){
-    if(marker){
+var placeMarker = function (location, map, marker) {
+    if (marker) {
         marker.setPosition(location);
     } else {
         marker = new google.maps.Marker({
@@ -49,20 +49,20 @@ var placeMarker = function(location, map, marker){
     }
     function toggleBounce() {
         if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
+            marker.setAnimation(null);
         } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
         }
     }
     return marker;
 }
 
-var showPosition = function(position){
+var showPosition = function (position) {
     this.latitud = position.coords.latitude;
-    this.longitud = position.coords.longitude; 
+    this.longitud = position.coords.longitude;
 };
 
-var displayError =  function(error) {
+var displayError = function (error) {
     var errors = {
         1: 'Permission denied',
         2: 'Position unavailable',
@@ -72,7 +72,7 @@ var displayError =  function(error) {
 };
 
 //get current position from navigator
-var getGeoLocation =  function(){
+var getGeoLocation = function () {
     if (navigator.geolocation) {
         var timeoutVal = 10 * 1000 * 1000;
         navigator.geolocation.getCurrentPosition(
@@ -91,13 +91,13 @@ function GmapsInit() {
     var marker = null;
     var pos = null;
 
-    if(comparethis !== ""){
+    if (comparethis !== "") {
         latitud = parseFloat(lat.value);
         longitud = parseFloat(lng.value);
     }
     pos = new google.maps.LatLng(latitud, longitud);
     asigLatLng(pos);
-  
+
     var mapOptions = {
         zoom: 17,
         center: new google.maps.LatLng(latitud, longitud),
@@ -112,7 +112,7 @@ function GmapsInit() {
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     var searchBox = new google.maps.places.SearchBox((input));
-    google.maps.event.addListener(searchBox, 'places_changed', function(){
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
 
         var places = searchBox.getPlaces();
 
@@ -148,6 +148,6 @@ function GmapsInit() {
         asigLatLng(event.latLng);
     });
     google.maps.event.addListener(marker, 'dragend', function (event) {
-         asigLatLng(event.latLng);
+        asigLatLng(event.latLng);
     });
 }
